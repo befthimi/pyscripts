430ebf0d1df161d7ef4302294bc9ab4989e93624
befthimi <bill_efthimiou@hotmail.com>
2018-03-25 20:01:49 +1100

    changed striing formatting

--- google_python_exercises/string1.py
+++ google_python_exercises/string1.py
@@ -81,7 +81,8 @@ def test(got, expected):
     prefix = ' OK '
   else:
     prefix = '  X '
-  print ('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+  print (f'{prefix} got: {repr(got)} expected: {repr(expected)}')
+#  print ('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
 #  print ('%s got: %s expected: %s') % (prefix, repr(got), repr(expected))
 
 # Provided main() calls the above functions with interesting inputs,
